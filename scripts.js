33119b677957373d87efe2762c0eeba32e0857d8
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-01-04 08:44:35 -0700

    Finished Basic Calculator Functions
    Knowing that the scripting logic will undoubtedly become more complex,
    I have written the basic add subtract divide and multiply functions that
    in theory I will be able to use later on. My suspicion is that I will
    have to make the numbers pressed be added to an array with an event
    listener, then store the operation, then add the second numbers to the
    array, then once the user hits the equals sign, have the arrays joined
    in such a way that the return value is the number they are looking for.
    It sounds challenging but doable.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 // Testing external link
 console.log("testing 123");
+
+// Functions for basic math operations
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1 / num2
+}
